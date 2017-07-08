6c9eecb62153eba28182724c72919e3486aa38bb
Tomas Alejandro V. Falgui III <tomyfalguiiii@yahoo.com>
2017-07-08 16:31:46 +0800

    added contact us

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ export default () =>
   <div>
     Hi This is the home page.
     <Link to="/about/">About Page</Link>
-    <form action="/thankyou/" netlify>
+    <form name="contact" netlify>
       <input type="text" name="name" placeholder="name" />
       <input type="text" name="email" placeholder="email" />
       <button type="submit">Submit </button>
